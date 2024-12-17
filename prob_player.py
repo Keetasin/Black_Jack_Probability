d6ab0478cd1ab41a92113510a776e5dba425f685
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-12-18 02:30:38 +0700

    basic HiLo system

--- prob_player.py
+++ prob_player.py
@@ -5,7 +5,6 @@ def create_deck():
     deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
     return deck
 
-
 # HI-LO system
 def update_running_count(card):
     
@@ -15,32 +14,24 @@ def update_running_count(card):
         running_count -= 1
     elif card.isdigit() and 2 <= int(card) <= 6:
         running_count += 1
- 
-
     
 if __name__ == "__main__":
     
     running_count = 0 
-    score_dealer = 0
-    score_player = 0
-
-    player_hand = []
-    dealer_hand = []
 
     deck = create_deck()
     print(deck)
+    for i in range(52):
 
-    card = random.choice(deck)
-    deck.pop(deck.index(card))
-    print(len(deck))
-    print(deck)
-    print("Card :", card)
-    print("Point :", running_count) 
-    update_running_count(card)
-    print("Point :", running_count)
+        card = random.choice(deck)
+        deck.pop(deck.index(card))
+        print("Card :", card)
+        print("Point :", running_count) 
+        update_running_count(card)
+        print("Point :", running_count)
 
-    if running_count > 0:
-        print(f"{running_count} high cards left in the deck, dealer's chance of busting when drawing additional cards.")
-    elif running_count < 0:
-        print(f"{running_count} low cards left in the deck, increasing Blackjack or a high hand, reducing the dealer's chance of busting.")    
-    
+        if running_count > 0:
+            print(f"{running_count} high cards left in the deck, dealer's chance of busting when drawing additional cards.")
+        elif running_count < 0:
+            print(f"{running_count} low cards left in the deck, increasing Blackjack or a high hand, reducing the dealer's chance of busting.")    
+        print("left card :", len(card))
